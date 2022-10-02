007685e274b915b9059a39044840bb4a6961097d
eslamicoo912 <eslamicoo3@gmail.com>
2022-10-02 11:28:14 +0200

    create the card

---
+++ Task 01 Todo List/src/components/Card.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  MdOutlineDoneOutline,
+  MdEdit,
+  MdOutlineDeleteForever,
+} from "react-icons/md";
+
+const Card = ({ title, completedClick, editClick, deleteClick }) => {
+  return (
+    <div>
+      <div className="title">
+        <p>{title}</p>
+      </div>
+      <div className="btns-container">
+        <button className="completed" onClick={completedClick}>
+          <MdOutlineDoneOutline />
+        </button>
+        <button className="edit" onClick={editClick}>
+          <MdEdit />
+        </button>
+        <button className="delete" onClick={deleteClick}>
+          <MdOutlineDeleteForever />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
