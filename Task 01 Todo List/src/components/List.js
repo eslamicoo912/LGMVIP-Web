644f5c67b7fe28d6d849bd9fdc67fc7f4334107d
eslamicoo912 <eslamicoo3@gmail.com>
2022-10-02 12:14:37 +0200

    complete the list component

--- Task 01 Todo List/src/components/List.js
+++ Task 01 Todo List/src/components/List.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Card from "./Card";
 
-const List = ({ tasks }) => {
-  return <div>{tasks[0].title}</div>;
+const List = ({ tasks, completedClick, editClick, deleteClick }) => {
+  const tasksList = tasks.map((task, index) => {
+    return (
+      <Card
+        key={index}
+        title={task}
+        completedClick={completedClick}
+        editClick={editClick}
+        deleteClick={deleteClick}
+      />
+    );
+  });
+
+  return <div>{tasksList}</div>;
 };
 
 export default List;
