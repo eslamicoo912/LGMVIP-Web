f3665638fbc9d99341bba262fe5f99a3ebd13125
eslamicoo912 <eslamicoo3@gmail.com>
2022-10-05 11:28:46 +0200

    create loading functionality

--- Task 02 User Card/src/App.js
+++ Task 02 User Card/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Card from "./components/Card";
 import "./App.css";
 
@@ -6,13 +6,20 @@ const URL = "https://reqres.in/api/users?page=1";
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState("null");
 
   const fetchData = async () => {
+    setLoading("true");
     const response = await fetch(URL);
     const data = await response.json();
     const { data: usersData } = data;
     setUsers(usersData);
+    setTimeout(() => {
+      setLoading("null");
+    }, 1000);
   };
+
+  if (loading === "true") return <h1 className="loading">Loading...</h1>;
 
   return (
     <div className="app">
