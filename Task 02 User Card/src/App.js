30cc042e4ab2bfe7273ce2cba8b7f1155390c5e5
eslamicoo912 <eslamicoo3@gmail.com>
2022-10-05 10:52:11 +0200

    create the fetchData functionality

--- Task 02 User Card/src/App.js
+++ Task 02 User Card/src/App.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const URL = "https://reqres.in/api/users?page=1";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const { data: usersData } = data;
+    setUsers(usersData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(users);
+
   return <div>App</div>;
 };
 
