fb090cb2d1a845971dd114e3e3df0ba044732d90
eslamicoo912 <eslamicoo3@gmail.com>
2022-10-05 11:03:42 +0200

    create the getUsers button and its fuctionality

--- Task 02 User Card/src/App.js
+++ Task 02 User Card/src/App.js
@@ -5,25 +5,24 @@ const URL = "https://reqres.in/api/users?page=1";
 
 const App = () => {
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     const response = await fetch(URL);
     const data = await response.json();
     const { data: usersData } = data;
     setUsers(usersData);
-    setLoading(false);
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
   return (
     <div className="app">
-      <h1>Hello, users</h1>
-      <Card data={users} />
+      <header>
+        <h1>Hello, users</h1>
+        <button onClick={fetchData}>Get Users</button>
+      </header>
+
+      <div className="list">
+        <Card data={users} />
+      </div>
     </div>
   );
 };
